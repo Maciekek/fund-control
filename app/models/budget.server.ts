d2af7028323c3cae46a3b9a60c4f1b4751fa671e
maciejkucharski <maciej.kucharski@senti1.com>
2022-07-04 16:29:39 +0200

    working on CRuD of budgets, single budget template

--- app/models/budget.server.ts
+++ app/models/budget.server.ts
@@ -1,10 +1,9 @@
-import type { Password, User } from "@prisma/client";
-import bcrypt from "bcryptjs";
+import type { Password, User, Budget } from "@prisma/client";
 
 import { prisma } from "~/db.server";
-import { Note } from "@prisma/client";
 
 export type { User } from "@prisma/client";
+export type { Budget } from "@prisma/client";
 
 export async function getAllBudgets(email: User["email"]) {
   const user = await prisma.user.findUnique({
@@ -15,4 +14,42 @@ export async function getAllBudgets(email: User["email"]) {
   });
 
   return user?.budget;
+}
+
+export function deleteBudget({
+  id,
+  userId,
+}: Pick<Budget, "id"> & { userId: User["id"] }) {
+  return prisma.budget.deleteMany({
+    where: { id, userId },
+  });
+}
+
+export function createBudget({
+  name,
+  userId,
+}: Pick<Budget, "name"> & {
+  userId: User["id"];
+}) {
+  return prisma.budget.create({
+    data: {
+      name,
+      user: {
+        connect: {
+          id: userId,
+        },
+      },
+    },
+  });
+}
+
+export function getBudget({
+  id,
+  userId,
+}: Pick<Budget, "id"> & {
+  userId: User["id"];
+}) {
+  return prisma.budget.findFirst({
+    where: { id, userId },
+  });
 }
