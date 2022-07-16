bc1c1e8d32de2ca300f24015fd9b8b7f55d6331b
maciejkucharski <maciej.kucharski@senti1.com>
2022-07-16 20:50:23 +0200

    update run-prod

--- run-prod.sh
+++ run-prod.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 export NODE_ENV=production
-export REMIX_BASEPATH=budget-app
+#export REMIX_BASEPATH=budget-app
 export PORT=3002
 
-npm run start
+npm run build
+
+remix-serve build/
+#pm2 start dist/main.js --name="pi-home-server" --watch
