9567ebcc6e70f1ea267e66fc9a1b7258608a6c0b
maciej.kucharski <maciej.kucharski@senti1.com>
2023-06-12 14:09:25 +0200

    sort top table

--- app/components/widgets/topTable.tsx
+++ app/components/widgets/topTable.tsx
@@ -1,37 +1,49 @@
+import { formatNumberToCurrency } from "~/helpers/number";
+
 type TopTableProps = {
   total: number;
   data: { [key: string]: number };
 };
 
 const TopTable = ({ data, total }: TopTableProps) => {
+  const sortedData = Object.keys(data)
+    .map((key) => ({
+      key,
+      value: data[key],
+    }))
+    .sort((a, b) => a.value - b.value);
+
   return (
     <div className="rounded-lg bg-white p-4 shadow sm:p-6 xl:p-8 ">
       <h3 className="mb-10 text-xl font-bold leading-none text-gray-900">
-        Acquisition Overview
+        Outgoes summary
       </h3>
       <div className="block w-full overflow-x-auto">
         <table className="w-full border-collapse items-center bg-transparent">
           <thead>
             <tr>
               <th className="whitespace-nowrap border-l-0 border-r-0 bg-gray-50 px-4 py-3 text-left align-middle text-xs font-semibold uppercase text-gray-700">
-                Top Channels
+                Category
               </th>
               <th className="whitespace-nowrap border-l-0 border-r-0 bg-gray-50 px-4 py-3 text-left align-middle text-xs font-semibold uppercase text-gray-700">
-                Users
+                Amount
               </th>
-              <th className="min-w-140-px whitespace-nowrap border-l-0 border-r-0 bg-gray-50 px-4 py-3 text-left align-middle text-xs font-semibold uppercase text-gray-700" />
+              <th className="min-w-140-px whitespace-nowrap border-l-0 border-r-0 bg-gray-50 px-4 py-3 text-left align-middle text-xs font-semibold uppercase text-gray-700">
+                Percentage
+              </th>
             </tr>
           </thead>
           <tbody className="divide-y divide-gray-100">
-            {Object.keys(data).map((key: string) => {
-              const share = ((data[key] / total) * 100).toFixed(2);
+            {sortedData.map((data: { key: string; value: number }) => {
+              const share = ((data.value / total) * 100).toFixed(2);
+
               return (
-                <tr className="text-gray-500" key={key}>
+                <tr className="text-gray-500" key={data.key}>
                   <th className="whitespace-nowrap border-t-0 p-4 px-4 text-left align-middle text-sm font-normal">
-                    {key}
+                    {data.key}
                   </th>
                   <td className="whitespace-nowrap border-t-0 p-4 px-4 align-middle text-xs font-medium text-gray-900">
-                    {data[key]}
+                    {formatNumberToCurrency(data.value)}
                   </td>
                   <td className="whitespace-nowrap border-t-0 p-4 px-4 align-middle text-xs">
                     <div className="flex items-center">
@@ -49,112 +61,6 @@ const TopTable = ({ data, total }: TopTableProps) => {
                 </tr>
               );
             })}
-
-            {/*<tr className="text-gray-500">*/}
-            {/*  <th className="whitespace-nowrap border-t-0 p-4 px-4 text-left align-middle text-sm font-normal">*/}
-            {/*    Referral*/}
-            {/*  </th>*/}
-            {/*  <td className="whitespace-nowrap border-t-0 p-4 px-4 align-middle text-xs font-medium text-gray-900">*/}
-            {/*    4,025*/}
-            {/*  </td>*/}
-            {/*  <td className="whitespace-nowrap border-t-0 p-4 px-4 align-middle text-xs">*/}
-            {/*    <div className="flex items-center">*/}
-            {/*      <span className="mr-2 text-xs font-medium">24%</span>*/}
-            {/*      <div className="relative w-full">*/}
-            {/*        <div className="h-2 w-full rounded-sm bg-gray-200">*/}
-            {/*          <div*/}
-            {/*            className="h-2 rounded-sm bg-orange-300"*/}
-            {/*            style={{ width: "30%" }}*/}
-            {/*          />*/}
-            {/*        </div>*/}
-            {/*      </div>*/}
-            {/*    </div>*/}
-            {/*  </td>*/}
-            {/*</tr>*/}
-            {/*<tr className="text-gray-500">*/}
-            {/*  <th className="whitespace-nowrap border-t-0 p-4 px-4 text-left align-middle text-sm font-normal">*/}
-            {/*    Direct*/}
-            {/*  </th>*/}
-            {/*  <td className="whitespace-nowrap border-t-0 p-4 px-4 align-middle text-xs font-medium text-gray-900">*/}
-            {/*    3,105*/}
-            {/*  </td>*/}
-            {/*  <td className="whitespace-nowrap border-t-0 p-4 px-4 align-middle text-xs">*/}
-            {/*    <div className="flex items-center">*/}
-            {/*      <span className="mr-2 text-xs font-medium">18%</span>*/}
-            {/*      <div className="relative w-full">*/}
-            {/*        <div className="h-2 w-full rounded-sm bg-gray-200">*/}
-            {/*          <div*/}
-            {/*            className="h-2 rounded-sm bg-teal-400"*/}
-            {/*            style={{ width: "30%" }}*/}
-            {/*          ></div>*/}
-            {/*        </div>*/}
-            {/*      </div>*/}
-            {/*    </div>*/}
-            {/*  </td>*/}
-            {/*</tr>*/}
-            {/*<tr className="text-gray-500">*/}
-            {/*  <th className="whitespace-nowrap border-t-0 p-4 px-4 text-left align-middle text-sm font-normal">*/}
-            {/*    Social*/}
-            {/*  </th>*/}
-            {/*  <td className="whitespace-nowrap border-t-0 p-4 px-4 align-middle text-xs font-medium text-gray-900">*/}
-            {/*    1251*/}
-            {/*  </td>*/}
-            {/*  <td className="whitespace-nowrap border-t-0 p-4 px-4 align-middle text-xs">*/}
-            {/*    <div className="flex items-center">*/}
-            {/*      <span className="mr-2 text-xs font-medium">12%</span>*/}
-            {/*      <div className="relative w-full">*/}
-            {/*        <div className="h-2 w-full rounded-sm bg-gray-200">*/}
-            {/*          <div*/}
-            {/*            className="h-2 rounded-sm bg-pink-600"*/}
-            {/*            style={{ width: "30%" }}*/}
-            {/*          ></div>*/}
-            {/*        </div>*/}
-            {/*      </div>*/}
-            {/*    </div>*/}
-            {/*  </td>*/}
-            {/*</tr>*/}
-            {/*<tr className="text-gray-500">*/}
-            {/*  <th className="whitespace-nowrap border-t-0 p-4 px-4 text-left align-middle text-sm font-normal">*/}
-            {/*    Other*/}
-            {/*  </th>*/}
-            {/*  <td className="whitespace-nowrap border-t-0 p-4 px-4 align-middle text-xs font-medium text-gray-900">*/}
-            {/*    734*/}
-            {/*  </td>*/}
-            {/*  <td className="whitespace-nowrap border-t-0 p-4 px-4 align-middle text-xs">*/}
-            {/*    <div className="flex items-center">*/}
-            {/*      <span className="mr-2 text-xs font-medium">9%</span>*/}
-            {/*      <div className="relative w-full">*/}
-            {/*        <div className="h-2 w-full rounded-sm bg-gray-200">*/}
-            {/*          <div*/}
-            {/*            className="h-2 rounded-sm bg-indigo-600"*/}
-            {/*            style={{ width: "30%" }}*/}
-            {/*          ></div>*/}
-            {/*        </div>*/}
-            {/*      </div>*/}
-            {/*    </div>*/}
-            {/*  </td>*/}
-            {/*</tr>*/}
-            {/*<tr className="text-gray-500">*/}
-            {/*  <th className="whitespace-nowrap border-t-0 p-4 pb-0 text-left align-middle text-sm font-normal">*/}
-            {/*    Email*/}
-            {/*  </th>*/}
-            {/*  <td className="whitespace-nowrap border-t-0 p-4 pb-0 align-middle text-xs font-medium text-gray-900">*/}
-            {/*    456*/}
-            {/*  </td>*/}
-            {/*  <td className="whitespace-nowrap border-t-0 p-4 pb-0 align-middle text-xs">*/}
-            {/*    <div className="flex items-center">*/}
-            {/*      <span className="mr-2 text-xs font-medium">7%</span>*/}
-            {/*      <div className="relative w-full">*/}
-            {/*        <div className="h-2 w-full rounded-sm bg-gray-200">*/}
-            {/*          <div*/}
-            {/*            className="h-2 rounded-sm bg-purple-500"*/}
-            {/*            style={{ width: "30%" }}*/}
-            {/*          ></div>*/}
-            {/*        </div>*/}
-            {/*      </div>*/}
-            {/*    </div>*/}
-            {/*  </td>*/}
-            {/*</tr>*/}
           </tbody>
         </table>
       </div>
